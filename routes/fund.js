c6491e07029b1484e2626b078736347e9c68df60
ChandanMahima <73341073+mahima-chandan@users.noreply.github.com>
2020-12-13 00:29:29 +0530

    Update fund.js
    added a balance field

--- routes/fund.js
+++ routes/fund.js
@@ -2,16 +2,22 @@ const c = require("../config");
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+const { bets } = require("../data");
+const { users } = require("../data");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("fund", {});
+    let user = await users.getUserByName("mahima")
+        let amountBal = user.balance;
+        let name = user.username;
+        res.render("fund", {Balance:amountBal,Name:name});
   } catch (e) {
     res.status(400).send(`route: / ${e}`);
   }
 });
 
-router.post("/fund", async (req, res) => {
+router.post("/", async (req, res) => {
+  //updated,Mahima
 /* commented out doesn't compile, Dale
   try {
     let Balance = await valiData.updateBalance(
@@ -22,9 +28,9 @@ router.post("/fund", async (req, res) => {
 
 
         try {
-           let Balance = await valiData.updateBalance(req.body.cardname,req.body.funamot);
+           let Balance = await users.updateBalance(req.body.cardname,req.body.funamot);
             
-            res.render('success', { Balance: Balance })
+            res.render("bet", {});
         } catch (e) {
             res.sendStatus(500);
         }
@@ -32,6 +38,7 @@ router.post("/fund", async (req, res) => {
 
 
 /* Mahima to develop this
+//done,Mahima
 router.post('/', async (req, res) => {
   try {
     console.log(req.body);
