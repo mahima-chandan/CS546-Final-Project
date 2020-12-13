43b3932ae64b46e8aec0dca0ae31b3002d2d4085
Dale Pippert <dpippert@wsfsbank.com>
2020-12-13 12:07:44 -0500

    putting mahima's fixes back they seemed to be lost

--- routes/fund.js
+++ routes/fund.js
@@ -2,45 +2,28 @@ const c = require("../config");
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+const { bets } = require("../data");
+const { users } = require("../data");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("fund", {});
+    let user = await users.getUserByName("mahima")
+        let amountBal = user.balance;
+        let name = user.username;
+        res.render("fund", {Balance:amountBal,Name:name});
   } catch (e) {
     res.status(400).send(`route: / ${e}`);
   }
 });
 
-router.post("/fund", async (req, res) => {
-/* commented out doesn't compile, Dale
-  try {
-    let Balance = await valiData.updateBalance(
-      req.body.cardname,
-      req.body.funamot
-    );
-*/
-
-
+router.post("/", async (req, res) => {
         try {
-           let Balance = await valiData.updateBalance(req.body.cardname,req.body.funamot);
-            
-            res.render('success', { Balance: Balance })
+           let Balance = await users.updateBalance(req.body.cardname,req.body.funamot);
+            res.render("bet", {});
         } catch (e) {
+            console.log(e);
             res.sendStatus(500);
         }
     });
-
-
-/* Mahima to develop this
-router.post('/', async (req, res) => {
-  try {
-    console.log(req.body);
-    res.sendFile(path.resolve('public/bet.html'));
-  }
-  catch (e) {
-    res.status(400).send(`route: / ${e}`);
-  }
-});
-*/
 
 module.exports = router;
