9f7ba57ee9e303fb9c6f804e87920ede1286cf2e
cwettre2 <32401320+cwettre2@users.noreply.github.com>
2020-12-13 20:50:42 -0500

    updated, removed backend

--- public/bet.js
+++ public/bet.js
@@ -1,98 +1,14 @@
 const mongoCollections = require('../config/index');
-const lines = mongoCollections.lines;
+const allLine = mongoCollections.lines;
 const bets = mongoCollections.bets;
-
-async function addBet (gameId, aspBet, aspWin, aspCollect, amlBet, amlWin, amlCollect, hspBet, hspWin, hspCollect, hmlBet, hmlWin, hmlCollect, overBet, overWin, overCollect, underBet, underWin, underCollect) {
-    const allBets = await bets();
-    let newBet = {
-        gameId: gameId,
-        aspBet: aspBet,
-        aspWin: aspWin,
-        aspCollect: aspCollect, 
-        amlBet: amlBet,
-        amlWin: amlWin,
-        amlCollect: amlCollect,
-        hspBet: hspBet,
-        hspWin: hspWin,
-        hspCollect: hspCollect,
-        hmlBet: hmlBet,
-        hmlWin: hmlWin,
-        hmlCollect: hmlCollect,
-        overBet: overBet,
-        overWin: overWin,
-        overCollect: overCollect,
-        underBet: underBet,
-        underWin: underWin,
-        underCollect: underCollect
-    }
-
-    const betInfo = await allBets.insertOne(newBet);
-        if (betInfo.insertedCount != 1)
-            throw 'Failed to add bet';
-            
-    return true;
-
-}
+const lines = await this.getAllLines();
 
 async function getAllLines() {
-    const allLines = await lines();
+    const allLines = await allLine();
     const line = await allLines.find({}).toArray();
     if (line.length == 0)
         throw 'No lines to return';
     return line;
-}
-
-//submit bets to server
-let forms = document.getElementsByClassName('form');
-if (forms) {
-    for (let i = 0; i < forms.length; i++) {
-        forms[i].addEventListener('submit', event => {
-            let gameId = forms[i].id;
-            let aspBetId = "aspbet-" + gameId;
-            let aspWinId = "aspwin-" + gameId;
-            let aspCollectId = "aspcollect-" + gameId;
-            let amlBetId = "amlbet-" + gameId;
-            let amlWinId = "amlwin-" + gameId;
-            let amlCollectId = "amlcollect-" + gameId;
-            let hspBetId = "hspbet-" + gameId;
-            let hspWinId = "hspwin-" + gameId;
-            let hspCollectId = "hspcollect-" + gameId;
-            let hmlBetId = "hmlbet-" + gameId;
-            let hmlWinId = "hmlwin-" + gameId;
-            let hmlCollectId = "hmlcollect-" + gameId;
-            let overBetId = "overbet-" + gameId;
-            let overWinId = "overwin-" + gameId;
-            let overCollectId = "overcollect-" + gameId;
-            let underBetId = "underbet-" + gameId;
-            let underWinId = "underwin-" + gameId;
-            let underCollectId = "undercollect-" + gameId;
-
-            let aspBet = document.getElementById(aspBetId);
-            let aspWin = document.getElementById(aspWinId);
-            let aspCollect = document.getElementById(aspCollectId);
-            let amlBet = document.getElementById(amlBetId);
-            let amlWin = document.getElementById(amlWinId);
-            let amlCollect = document.getElementById(amlCollectId);
-            let hspBet = document.getElementById(hspBetId);
-            let hspWin = document.getElementById(hspWinId);
-            let hspCollect = document.getElementById(hspCollectId);
-            let hmlBet = document.getElementById(hmlBetId);
-            let hmlWin = document.getElementById(hmlWinId);
-            let hmlCollect = document.getElementById(hmlCollectId);
-            let overBet = document.getElementById(overBetId);
-            let overWin = document.getElementById(overWinId);
-            let overCollect = document.getElementById(overCollectId);
-            let underBet = document.getElementById(underBetId);
-            let underWin = document.getElementById(underWinId);
-            let underCollect = document.getElementById(underCollectId);
-
-            await addBet(gameId, aspBet, aspWin, aspCollect, amlBet, amlWin, amlCollect, hspBet, hspWin, hspCollect, hmlBet, hmlWin, hmlCollect, overBet, overWin, overCollect, underBet, underWin, underCollect);
-
-
-        }
-        );
-        forms[i].addEventListener
-    }
 }
 
 //ASP bet
