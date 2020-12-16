38f4b0fa3fe36944b166de351fe592c8fbe22476
Dale Pippert <dpippert@wsfsbank.com>
2020-12-15 21:21:54 -0500

    checkpoint

--- routes/history.js
+++ routes/history.js
@@ -9,8 +9,7 @@ router.get('/', async (req, res) => {
     const cur = await bets.getByUserId("aeeb758c75ee6029745ca8a0");
     const x = await cur.toArray();
     res.render('history', {bets: x, cssOverrides: "history.css"}); 
-  }
-  catch (e) {
+  } catch (e) {
     res.status(500).send(`route: / ${e}`);
   }
 });
